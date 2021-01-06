893d3e951528dce3bd8c864caf3a7349fde6c73f
y <1976954875@qq.com>
2021-01-06 17:37:56 +0800

    update api

--- param_client/src/client.cpp
+++ param_client/src/client.cpp
@@ -11,16 +11,19 @@ void callback(param_server::SimpleType &config)
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "param_client_node");
-
+  //package config
   param_server::Server server("param_client","cfg/config.yml");
-  //set callback
-  //auto f = boost::bind(&callback, _1);
   param_server::CallbackType f = boost::bind(&callback, _1); //绑定回调函数
   server.setCallback(f);
   bool b_value;
   float f_value;
-  server.get("boolean-value",b_value);
-  server.get("floating-value",f_value);
+  if(server.exist("boolean-value")){
+    server.get("boolean-value",b_value);
+  }
+  if(server.exist("floating-value")){
+    server.get("floating-value",f_value);
+  }
+  std::cout << "b_value:" << b_value << std::endl;
   std::cout << "f_value:" << f_value << std::endl;
   ros::spin();
 
